e51ffa226f307f15ba477bdb2d73f3dae47f3708
ppopow <pawel.popow@gmail.com>
2020-10-19 19:48:50 +0200

    nowe zadanie38

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function numer (numer3, numer5) {
+    let tree = numer3 * numer5;
+    if(liczba3+liczba5) {
+        console.log("Wynik dodawania wynosi x");
+    } else if(liczba1-liczba2) {
+        console.log("Wynik odejmowania wynosi x");
+    } else {
+        console.log("Wynik mnożenia wynosi x");
+    }
+    return x;
+}
+
